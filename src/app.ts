5a13a64552667a82d97199677383bfe1799c74f4
Dariusz Filipiak <darekf77@gmail.com>
2024-11-17 07:51:37 +0100

    release:  v18.0.14

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 //#region @notForNpm
 declare const ENV: any;
 
-
-async function start() {
-
-}
+async function start() {}
 
 export default start;
 
+//#endregion
+
+//#region  incremental-compiler component
+//#region @browser
+@Component({ template: 'hello world fromr incremental-compiler' })
+export class IncrementalCompilerComponent {}
+//#endregion
+//#endregion
+
+//#region  incremental-compiler module
+//#region @browser
+@NgModule({
+  declarations: [IncrementalCompilerComponent],
+  imports: [CommonModule],
+  exports: [IncrementalCompilerComponent],
+})
+export class IncrementalCompilerModule {}
+//#endregion
 //#endregion
