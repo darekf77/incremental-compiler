bf6a6136682bc2f6a01e3c0137c125469c0465b2
Dariusz Filipiak <darekf77@gmail.com>
2025-03-21 15:14:19 +0100

    fix: browser ready

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,12 +1,20 @@
-export const COMPILER_POOLING = (process.platform === 'win32') ? {
-  // TODO when not in admin mode vscode on windows -> problem with moving/deleting files inside vscode
+
+
+ let COMPILER_POOLING = {};
+
+ //#region @backend
+if(process.platform === 'win32') {
+ COMPILER_POOLING = {
+   // TODO when not in admin mode vscode on windows -> problem with moving/deleting files inside vscode
   // usePolling: true,
   // interval: 1000,
   // binaryInterval: 2000,
-} : {};
-// console.log({
-//   COMPILER_POOLING
-// })
+ }
+}
+//#endregion
+
+export {COMPILER_POOLING};
+
 
 export const IGNORE_BY_DEFAULT = [
   '**/node_modules/**/*.*',
