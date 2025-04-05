099856dbd5596df893495cd9f1d4526b66d6747c
Dariusz Filipiak <darekf77@gmail.com>
2025-04-05 22:08:50 +0200

    refactor: env options

---
+++ env.ts
@@ -0,0 +1,8 @@
+import type { EnvOptions } from 'tnp/src';
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'incremental-compiler.example.domain.com',
+    title: 'Incremental Compiler',
+  },
+};
+export default env;
