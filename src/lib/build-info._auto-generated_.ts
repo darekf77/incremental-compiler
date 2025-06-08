5c759c66c060172f7c8ac12da0a8409340c88270
Dariusz Filipiak <darekf77@gmail.com>
2025-06-08 20:06:42 +0200

    chore: update

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,5 +1,6 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
+export const APP_ID = 'com.domain.example.incremental-compiler';
 export const PROJECT_NPM_NAME = 'incremental-compiler';
 export const CURRENT_PACKAGE_VERSION = '19.0.29';
 // THIS FILE IS GENERATED - DO NOT MODIFY
