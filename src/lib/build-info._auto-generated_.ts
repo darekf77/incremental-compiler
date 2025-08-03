f80e7fa91eb82d42b3d8b97d42ae70e3c2eb4af2
Dariusz Filipiak <darekf77@gmail.com>
2025-08-03 21:22:00 +0200

    release: v19.0.42

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,7 +1,8 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const APP_ID = 'com.domain.example.incremental-compiler';
+export const BUILD_BASE_HREF = '';
 export const PROJECT_NPM_NAME = 'incremental-compiler';
-export const CURRENT_PACKAGE_VERSION = '19.0.41';
+export const CURRENT_PACKAGE_VERSION = '19.0.42';
 // THIS FILE IS GENERATED - DO NOT MODIFY
       
