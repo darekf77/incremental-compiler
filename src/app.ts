3123a1dd1622c85e49ad24eb4e6863a35c155503
Dariusz Filipiak <darekf77@gmail.com>
2026-01-20 17:15:06 +0100

    chore: api update

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,26 @@
-// @ts-nocheck
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
+// // @ts-nocheck
+// /* */
+// /* */
+// /* */
+// /* */
+// /* */
+// /* */
+// /* */
 
-//#region  incremental-compiler component
-//#region @browser
-@Component({ template: 'hello world fromr incremental-compiler' })
-export class IncrementalCompilerComponent {}
-//#endregion
-//#endregion
+// //#region  incremental-compiler component
+// //#region @browser
+// @Component({ template: 'hello world fromr incremental-compiler' })
+// export class IncrementalCompilerComponent {}
+// //#endregion
+// //#endregion
 
-//#region  incremental-compiler module
-//#region @browser
-@NgModule({
-  declarations: [IncrementalCompilerComponent],
-  imports: [CommonModule],
-  exports: [IncrementalCompilerComponent],
-})
-export class IncrementalCompilerModule {}
-//#endregion
-//#endregion
+// //#region  incremental-compiler module
+// //#region @browser
+// @NgModule({
+//   declarations: [IncrementalCompilerComponent],
+//   imports: [CommonModule],
+//   exports: [IncrementalCompilerComponent],
+// })
+// export class IncrementalCompilerModule {}
+// //#endregion
+// //#endregion
